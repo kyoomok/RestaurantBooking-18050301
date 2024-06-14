bb5819787696985daad4eb14825c3d50ef903258
km4.kim <kyoomok@naver.com>
2024-06-14 11:24:15 +0900

    [feature] step1

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -1,10 +1,20 @@
+from datetime import datetime
 import unittest
+
+from booking_scheduler import BookingScheduler
+from schedule import Customer, Schedule
 
 
 class TestBookingScheduler(unittest.TestCase):
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
-        pass
+        not_on_the_hour = datetime.strptime("2024/06/14 11:20", "%Y/%m/%d %H:%M")
+        customer = Customer(name="no_name", phone_number="010-1234-1111")
+        schedule = Schedule(not_on_the_hour, 1, customer)
+        booking_scheduler = BookingScheduler(3)
+
+        with self.assertRaises(ValueError):
+            booking_scheduler.add_schedule(schedule)
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         pass
